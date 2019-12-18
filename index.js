35d7acc84f157e94939794ecc6aa6a4fbd3f7c5a
Mursaleen Taqi <leemursa@gmail.com>
2019-12-18 06:57:06 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,26 @@
+const farmAnimals = 'cow horse sheep pig chicken'
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+const muppet = {
+  name: 'Kermit',
+  color: 'green',
+  song: 'The Rainbow Connection',
+  job: 'Host of The Muppet Show',
+  partner: 'Miss Piggy'
+};
+
+const k_muppet = {
+  k_name: 'Kermit',
+  k_color: 'green',
+  k_album: {
+    the_muppet_movie: {
+      song_1: 'Rainbow Connection',
+      song_2: 'Moving Right Along',
+      song_3: 'Never Before, Never Again',
+      song_4: 'I Hope That Something Better Comes Along',
+    },
+  },
+  k_job: 'Host of The Muppet Show',
+  k_partner: 'Miss Piggy'
+};
